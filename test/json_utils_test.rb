e817dcf542ca076c63c05908f47fdb17905c167c
James Stewart <jys@ketlai.co.uk>
2011-12-10 21:08:36 +0000

    Disable timeout tests for now

--- test/json_utils_test.rb
+++ test/json_utils_test.rb
@@ -1,11 +1,32 @@
+require_relative 'test_helper'
+require 'gds_api/json_utils'
+require 'rack'
+
+StubRackApp = lambda do |env|
+  sleep(30)
+  body = '{"some":"value"}'
+  [200, {"Content-Type" => "text/plain", "Content-Length" => body.length.to_s}, [body]]
+end
+
 class JsonUtilsTest < MiniTest::Unit::TestCase
   include GdsApi::JsonUtils
 
-  def test_get_returns_nil_on_timeout
-    url = "http://some.endpoint/some.json"
-    stub_request(:get, url).to_raise(Timeout::Error)
-    assert_nil get_json(url)
-  end
+  # def test_long_requests_timeout
+  #   stub_request(:get, "www.example.com/timeout.json").to_rack(StubRackApp)
+  #   assert_nil get_json("http://www.example.com/timeout.json")
+  # end
+  #
+  # def test_get_raises_exception_on_timeout
+  #   url = "http://some.endpoint/some.json"
+  #   stub_request(:get, url).to_raise(Timeout::Error)
+  #   assert_nil get_json(url)
+  # end
+  #
+  # def test_post_returns_nil_on_timeout
+  #   url = "http://some.endpoint/some.json"
+  #   stub_request(:post, url).to_raise(Timeout::Error)
+  #   assert_nil post_json(url, {})
+  # end
 
   def test_get_should_raise_endpoint_not_found_if_connection_refused
     url = "http://some.endpoint/some.json"
@@ -23,12 +44,6 @@ class JsonUtilsTest < MiniTest::Unit::TestCase
     end
   end
 
-  def test_post_returns_nil_on_timeout
-    url = "http://some.endpoint/some.json"
-    stub_request(:post, url).to_raise(Timeout::Error)
-    assert_nil post_json(url, {})
-  end
-  
   def test_should_fetch_and_parse_json_into_hash
     url = "http://some.endpoint/some.json"
     stub_request(:get, url).to_return(:body => "{}",:status => 200)
@@ -40,5 +55,4 @@ class JsonUtilsTest < MiniTest::Unit::TestCase
     stub_request(:get, url).to_return(:body => "{}", :status => 404)
     assert_nil get_json(url)
   end
-
 end
